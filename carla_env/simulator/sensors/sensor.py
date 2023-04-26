672e565cc6872f5c0ac14ebc96b4666482a266a7
doldam0 <jinustar19@gmail.com>
2023-04-26 11:52:39 +0900

    Add `listen` method to `Sensor`

--- carla_env/simulator/sensors/sensor.py
+++ carla_env/simulator/sensors/sensor.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import carla
 from typing_extensions import override
 
@@ -8,3 +10,6 @@ class Sensor(Actor[carla.Sensor]):
     @override
     def spawn(self, transform: carla.Transform, parent: Actor):
         return super().spawn(transform=transform, attach_to=parent)
+
+    def listen(self, callback: Callable[[carla.SensorData], None]):
+        self.actor.listen(callback)
